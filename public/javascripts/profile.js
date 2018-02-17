0eb8262a86cd4f766d851be9c367a1d81edd1105
Oleg7735 <Olleg375@gmail.com>
2018-02-17 19:42:14 +0300

    Добавлен аватар(в профиль), загрузка аватара напрямую в s3, присвоение аватара по умолчанию.

---
+++ public/javascripts/profile.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function(){
+    var fileLoader = document.getElementById('newAvatar');
+    fileLoader.onchange = function(evt){
+        var tgt = evt.target || window.event.srcElement;
+        var files = tgt.files;
+        if(FileReader && files && files.length){
+            var fr = new FileReader();
+            fr.onload = function(){
+                document.getElementById('avatar').src = fr.result;
+            };
+            fr.readAsDataURL(files[0]);
+        }
+    }
+});
--- routes/chat.js
+++ routes/chat.js
@@ -2,20 +2,25 @@ var express = require('express');
 var router = express.Router();
 var db = require('../lib/database');
 var bCrypt = require('../lib/bCryptFunctions');
+var config = require('../config');
+var upload = require('../lib/multers3-upload').getUpload();
 var authorized = function(req, res, next){
     if(req.isAuthenticated()){
         return next();
     }
     res.redirect('login');
 };
-
+var createS3ObjectUrl = function(objectName){
+    return 'http://'+config.bucketName+'.s3.amazonaws.com/'+objectName;
+};
 /* GET users listing. */
 router.use(authorized);
 router.get('/', function(req, res) {
     res.render('chat', { userName: req.user.name });
 });
 router.get('/profile', function(req, res){
-    res.render('profile', {login:req.user.name, email:req.user.email});
+    var avatar = createS3ObjectUrl(req.user.avatar);
+    res.render('profile', {login:req.user.name, email:req.user.email, avatarPath:avatar});
 });
 router.post('/profile', function(req, res){
     var user = {
@@ -23,26 +28,29 @@ router.post('/profile', function(req, res){
         name:req.body.login,
         email:req.body.email
     };
+    var avatar = createS3ObjectUrl(req.user.avatar);
     db.updateUserWithoutPassword(user, function(){
-        res.render('profile', {login:req.body.login, email:req.body.email, message:'Профль успешно обновлен.'});
+            res.render('profile', {login:req.body.login, email:req.body.email, avatarPath:avatar, message:'Профль успешно обновлен.'});
     },
     function(error){
+        //TODO: можно просто перенаправлять на profile указывая ошибку в req.flech, а в profile учитывать ошибку
         console.log('Ошибка обновления профиля пользователя: '+ error);
-        res.render('profile', {login:req.user.name, email:req.user.email, message:'Не удалось обновить профиль. ' +
+        res.render('profile', {login:req.user.name, email:req.user.email, avatarPath:avatar, message:'Не удалось обновить профиль. ' +
             'Попробуйте снова.'});
     });
 
 });
 router.post('/password', function(req, res){
+    var avatar = createS3ObjectUrl(req.user.avatar);
     var oldpassword = req.body.oldPassword;
     var newPassword = req.body.newPassword;
     var newPasswordConfirm = req.body.newPasswordConfirm;
     if(! bCrypt.isPasswordCorrect(oldpassword, req.user.password)){
-        return res.render('profile', {login:req.user.name, email:req.user.email, passwordMessage:'Старый пароль' +
+        return res.render('profile', {login:req.user.name, email:req.user.email, avatarPath:avatar, passwordMessage:'Старый пароль' +
             ' был введен не правильно. Пароль не был изменен.'});
     }
     if(newPassword !== newPasswordConfirm){
-        return res.render('profile', {login:req.user.name, email:req.user.email, passwordMessage:'Новый пароль' +
+        return res.render('profile', {login:req.user.name, email:req.user.email, avatarPath:avatar, passwordMessage:'Новый пароль' +
             ' и подтверждение пароля не совпадают. Пароль не был изменен.'});
     }
     var user = {
@@ -50,16 +58,21 @@ router.post('/password', function(req, res){
         id: req.user.id
     };
     db.updateUserPassword(user, function(){
-        res.render('profile', {login:req.user.name, email:req.user.email, passwordMessage:'Новый пароль' +
+        res.render('profile', {login:req.user.name, email:req.user.email, avatarPath:avatar, passwordMessage:'Новый пароль' +
             ' был успешно установлен.'});
     },
     function(error){
         console.log('Ошибка смены пароля: ' + error);
-        res.render('profile', {login:req.user.name, email:req.user.email, passwordMessage:'Не удалось изменить' +
+        res.render('profile', {login:req.user.name, email:req.user.email,avatarPath:avatar, passwordMessage:'Не удалось изменить' +
             ' пароль'});
     });
 
 });
-
+router.post('/avatar', upload.single('avatar'), function (req, res) {
+    var avatar = createS3ObjectUrl(req.user.avatar);
+    console.log('avatar posted');
+    res.render('profile', {login:req.user.name, email:req.user.email, avatarPath:avatar});
+    //TODO:Ограничивать размер файла
+});
 
 module.exports = router;
