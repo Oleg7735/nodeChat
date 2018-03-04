5e819f3f535302d46f2e48ddec93333ee89284ed
Oleg7735 <Olleg375@gmail.com>
2018-03-04 21:35:18 +0300

    Версия для production  в aws

--- app.js
+++ app.js
@@ -44,8 +44,10 @@ switch(app.get('env')){
         break;
     }
     case 'production':{
-        dbConnectionString = process.env.DATABASE_URL;
-        //'postgresql://'+process.env.RDS_USERNAME+':'+process.env.RDS_PASSWORD+'@'+process.env.RDS_HOSTNAME+':'+process.env.RDS_PORT;
+        //for Heroku
+        //dbConnectionString = process.env.DATABASE_URL;
+        dbConnectionString = 'postgresql://'+process.env.RDS_USERNAME+':'+process.env.RDS_PASSWORD+'@'+process.env.RDS_HOSTNAME+':'+process.env.RDS_PORT+'/'+process.env.RDS_DB_NAME;
+        //dbConnectionString = 'postgresql://oltest:otW6bemnp@chatdb.co0fgmovb804.us-east-2.rds.amazonaws.com:5432/ebdb';
         break;
     }
 }
